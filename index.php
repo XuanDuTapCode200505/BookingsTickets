e4ad290d298b7ebd9c43c4a9c5abed13be09dc94
Cao Xuân Dự <pherocaodu@gmail.com>
2025-05-26 23:28:19 +0700

    crate project bookings flim

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>PhimChill</title>
+</head>
+<body>
+    <div class="container">
+        <?php include 'pages/layout/header.php'; ?>
+        <?php include 'pages/layout/menu.php'; ?>
+        <div id="content">
+            <div class="sidebar">
+                <p>sidebar</p>
+            </div>
+            <div class="main-content">
+                <p>main</p>
+            </div>
+        </div>
+        <div class="clear"></div>
+        <div class="footer">
+            <p>footer</p>
+        </div>
+    </div>
+</body>
+</html>
---
+++ pages/layout/menu.php
@@ -0,0 +1,16 @@
+<div class="menu">
+            <div class="menu-left">
+                <div class="logo">
+                    <a href="index.php"><img src="https://www.cgv.vn/skin/frontend/cgv/default/images/cgvlogo.png" alt="logo"></a>
+                </div>
+            </div>
+            <div class="menu-right">
+                <ul class="list-menu">
+                    <li><a href="#">Phim</a></li>
+                    <li><a href="#">Rạp</a></li>
+                    <li><a href="#">Tin tức</a></li>
+                    <li><a href="dangnhap.php">Đăng nhập</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="clear"></div>
